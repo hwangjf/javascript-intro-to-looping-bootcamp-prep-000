fb7e037bf9b1804a066fc7a533b30df7bbad7dbc
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-03-21 00:06:10 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+function forLoop (array) {
+  for (var i = 0; i < 25; i++) {
+  if (i === 1) {
+    array.push(console.log ("I am "+ i + " strange loop."))
+} else {
+  array.push(console.log ("I am "+ i + " strange loops."))
+}
+}
+}
